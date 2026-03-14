880e5c437a038817d39c41a0b534fa42f04bbca7
ScreteMonge <scretemonge@gmail.com>
2026-03-14 10:58:17 -0700

    v2.1.7 - POH bugfix - Updated POH region IDs

--- src/main/java/com/creatorskit/programming/MovementManager.java
+++ src/main/java/com/creatorskit/programming/MovementManager.java
@@ -17,7 +17,7 @@ public class MovementManager
     private Client client;
     private CreatorsConfig config;
     private PathFinder pathFinder;
-    private static final int[] POH_REGIONS = new int[]{7257, 7513, 7514, 7769, 7770, 8025, 8026};
+    private static final int[] POH_REGIONS = new int[]{7534, 7535, 7790, 7791, 8046, 8047, 8302, 8303};
     private static final int[] GAUNTLET_REGIONS = new int[]{7512, 7768};
 
     @Inject
